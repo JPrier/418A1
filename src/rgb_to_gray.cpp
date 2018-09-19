ea54cdeaec0cf8bc725c19378797fda6ccee30b6
JPrier <JoshKPrier@gmail.com>
2018-09-19 14:36:36 -0400

    Added rgb to gray

--- src/rgb_to_gray.cpp
+++ src/rgb_to_gray.cpp
@@ -7,9 +7,9 @@ void rgb_to_gray(
   std::vector<unsigned char> & gray)
 {
   gray.resize(height*width);
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  for (int i = 0; i < height*width*3-3; i += 3) {
+  	gray[i] = 0.2126*rgb[i] + 0.7152*rgb[i+1] + 0.0722*rgb[i+2];
+  }
 }
 
 
