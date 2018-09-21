ff0e3d1b5f399ff7d518adb1d03c7c7bc801c23e
JPrier <JoshKPrier@gmail.com>
2018-09-21 13:18:24 -0400

    changed rotate to test again

--- src/rotate.cpp
+++ src/rotate.cpp
@@ -9,14 +9,14 @@ void rotate(
   std::vector<unsigned char> & rotated)
 {
   int intarray[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-  int inp[9];
   rotated.resize(height*width*num_channels);
   for(int y = 0; y < 3; y++) {
   	for (int x = 0; x < 3; x++) {
     		int index =  x + (3*y-1);
-      	for (int n = 0; n < 1; n++) {
-  			     inp[n+1*index] = intarray[n+1*(y+3*x)];
-  		}
+        printf("%d\n", intarray[y+3*x]);
+      	//for (int n = 0; n < num_channels; n++) {
+  			     //rotated[n+num_channels*index] = input[n+num_channels*(y+3*x)];
+  		//}
   	}
   }
 }
