50493780389c56a6011af1bb6c1a9446ff695fb3
JPrier <JoshKPrier@gmail.com>
2018-09-21 12:42:17 -0400

    rotate syntax fix and added simulate_bayer_mosaic

--- src/rotate.cpp
+++ src/rotate.cpp
@@ -9,11 +9,11 @@ void rotate(
 {
   rotated.resize(height*width*num_channels);
   for(int y = 0; y < height; y++) {
-	for (int x = 0; x < width; x++) {
-  		int index =  x + (height*y-1);
-    		for (int n = 0; n < num_channels; n++) {
-			rotated[n+num_channels*index] = input[n+num_channels*(y+width*x)];
-		}
-	}
+  	for (int x = 0; x < width; x++) {
+    		int index =  x + (height*y-1);
+      	for (int n = 0; n < num_channels; n++) {
+  			     rotated[n+num_channels*index] = input[n+num_channels*(y+width*x)];
+  		}
+  	}
   }
 }
--- src/simulate_bayer_mosaic.cpp
+++ src/simulate_bayer_mosaic.cpp
@@ -7,7 +7,21 @@ void simulate_bayer_mosaic(
   std::vector<unsigned char> & bayer)
 {
   bayer.resize(width*height);
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  for (int x = 0; x < width; x++) {
+    for (int y = 0; y < height; y++) {
+      if (y % 2 == 0) {
+        if (x % 2 == 0) {
+          bayer[x+width*y] = rgb[1+3*(x+width*y)];
+        } else {
+          bayer[x+width*y] = rgb[2+3*(x+width*y)];
+        }
+      } else {
+        if (x % 2 == 0) {
+          bayer[x+width*y] = rgb[3*(x+width*y)];
+        } else {
+          bayer[x+width*y] = rgb[1+3*(X+width*y)];
+        }
+      }
+    }
+  }
 }
