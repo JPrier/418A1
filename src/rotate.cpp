dfab80c0cd39f12a4efffb577daf288306a4c0cf
JPrier <JoshKPrier@gmail.com>
2018-09-21 13:54:15 -0400

    returned rotate to normal

--- src/rotate.cpp
+++ src/rotate.cpp
@@ -8,15 +8,13 @@ void rotate(
   const int num_channels,
   std::vector<unsigned char> & rotated)
 {
-  int intarray[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
   rotated.resize(height*width*num_channels);
-  for(int y = 0; y < 3; y++) {
-  	for (int x = 0; x < 3; x++) {
-    		int index =  x + (3*y-1);
-        printf("%d\n", intarray[y+3*x]);
-      	//for (int n = 0; n < num_channels; n++) {
-  			     //rotated[n+num_channels*index] = input[n+num_channels*(y+3*x)];
-  		//}
+  for(int y = 0; y < height; y++) {
+  	for (int x = 0; x < width; x++) {
+    		int index =  x + (height*y-1);
+      	for (int n = 0; n < num_channels; n++) {
+  			     rotated[n+num_channels*index] = input[n+num_channels*(y+width*x)];
+  		}
   	}
   }
 }
